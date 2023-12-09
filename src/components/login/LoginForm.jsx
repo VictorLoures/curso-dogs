8f28e6dbd88d89dc89665ccda7cc871f70f13c58
Victor <victor.loures@projetusti.com.br>
2023-12-09 11:05:08 -0300

    estilizando login

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -4,6 +4,9 @@ import Input from "../forms/Input";
 import Button from "../forms/Buttons";
 import useForm from "../../hooks/useForm";
 import { UserContext } from "../../UserContext";
+import Error from "../helper/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../forms/Button.module.css";
 
 const LoginForm = () => {
   const username = useForm();
@@ -19,9 +22,9 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? (
@@ -29,9 +32,18 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link to="/login/perdeu" className={styles.perdeu}>
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastro</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link to="/login/criar" className={stylesBtn.button}>
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 };
