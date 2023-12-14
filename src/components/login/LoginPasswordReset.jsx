26259b178f2582b851e41cf11e4a631ac47c5728
Victor <victor.loures@projetusti.com.br>
2023-12-14 18:39:52 -0300

    perdeu a senha reset

--- src/components/login/LoginPasswordReset.jsx
+++ src/components/login/LoginPasswordReset.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Input from "../forms/Input";
+import Button from "../forms/Buttons";
 
 const LoginPasswordReset = () => {
-  return <div></div>;
+  return (
+    <section>
+      <h1 className="title">Resete a senha</h1>
+      <Input label="Nova senha" />
+      <Button>Enviar email</Button>
+    </section>
+  );
 };
 
 export default LoginPasswordReset;
