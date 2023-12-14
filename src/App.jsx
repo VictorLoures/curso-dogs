432e71f37a91adc1224e58c5b45edec1f86b4f38
Victor <victor.loures@projetusti.com.br>
2023-12-14 18:01:09 -0300

    perfil usuário e not found

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Photo from "./components/Photo/Photo";
+import UserProfile from "./components/user/UserProfile";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login";
 import { UserStorage } from "./UserContext";
 import User from "./components/user/User";
 import ProtectedRoute from "./components/helper/ProtectedRoute";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
@@ -20,6 +22,8 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
             <Route
               path="conta/*"
               element={
